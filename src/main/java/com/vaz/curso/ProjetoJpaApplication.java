b417257d0ebb30a1151eae40580997d778a518d8
vaz-27 <martriz4ever@gmail.com>
2021-10-20 19:51:54 -0300

    Projeto criado

---
+++ src/main/java/com/vaz/curso/ProjetoJpaApplication.java
@@ -0,0 +1,13 @@
+package com.vaz.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJpaApplication.class, args);
+	}
+
+}
